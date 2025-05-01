3d2800c28aab4acd35a29619ba1c6674a664108f
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-05-01 13:25:42 +0200

    Add ability to save and load state.

--- src/components/BlocklyCanvas.vue
+++ src/components/BlocklyCanvas.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <div ref="blocklyDiv" style="height: 70vh"></div>
+
+    <div class="flex flex-col justify-center items-center bg-gray-100">
+      <div class="my-4 flex space-x-4">
+        <button @click="blocklySave" class="bg-blue-600 text-white px-4 py-2 rounded shadow">
+          Save
+        </button>
+        <button @click="blocklyLoad" class="bg-red-600 text-white px-4 py-2 rounded shadow">
+          Load
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +34,31 @@ const supportedEvents = new Set([
   Blockly.Events.BLOCK_DELETE,
   Blockly.Events.BLOCK_MOVE,
 ]);
+
+const storageKey = 'tydiStreamVisBlockly';
+
+/** Saves the state of the workspace to browser's local storage. */
+function blocklySave() {
+  const _workspace = Blockly.getMainWorkspace();
+  // @ts-ignore There is some mismatch between the properties of the workspace object and the expected type.
+  const data = Blockly.serialization.workspaces.save(_workspace);
+  window.localStorage?.setItem(storageKey, JSON.stringify(data));
+  console.log("Saved state to local storage.");
+}
+
+function blocklyLoad() {
+  const data = window.localStorage?.getItem(storageKey);
+  if (!data) return;
+
+  const _workspace = Blockly.getMainWorkspace();
+
+  // Don't emit events during loading.
+  Blockly.Events.disable();
+  // @ts-ignore There is some mismatch between the properties of the workspace object and the expected type.
+  Blockly.serialization.workspaces.load(JSON.parse(data), _workspace, false);
+  Blockly.Events.enable();
+  console.log("Loaded state from local storage.");
+}
 
 function updateCode(event: any) {
   // Event objects extend `Blockly.Events.Abstract`, but there the type of the `type` property is string instead of an actual event type.
