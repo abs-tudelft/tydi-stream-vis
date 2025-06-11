fafbb8a00053275dc6f50fe9b8e2b8b82b1adc94
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-06-11 11:34:02 +0200

    Collect transfer item data sources correctly.
    Next step: produce the items themselves.

--- src/components/StreamVisualizer.vue
+++ src/components/StreamVisualizer.vue
@@ -38,20 +38,24 @@ const relativePaths = computed(() => {
 })
 
 function selectData(stack: any, path: (ObjectIndex | ArrayIndex)[]): any[] {
-  if (path.length === 0) return []
+  if (path.length === 0) return stack
   const lastLeaf = path.length === 1
   var newStack = (typeof stack ===  "string") ? Array.from(stack) : stack
 
   if (path[0] instanceof ArrayIndex) {
-    return (newStack as any[]).flatMap((item) => lastLeaf ? item : selectData(item, path.slice(1)))
+    if (newStack === null || newStack.map === undefined) {
+      // This can be the case for arrays with different types
+      return [newStack]
+    }
+    return (newStack as any[]).map((item) => lastLeaf ? item : selectData(item, path.slice(1)))
   }
-  return lastLeaf ? [newStack[path[0].name]] : selectData(newStack, path.slice(1))
+  return lastLeaf ? [newStack[path[0].name]] : selectData(newStack[path[0].name], path.slice(1))
 }
 
 const dataThings = computed(() => {
   return physicalStreams.value.map(s => {
     const path = s.dataPathList
-    return selectData(data.value, path)
+    return selectData(data.value, [...path, new ArrayIndex()])
   })
 })
 
@@ -59,15 +63,16 @@ const dataThings = computed(() => {
 
 <template>
   <div>Number of physical streams: {{physicalStreams?.length ?? 0}}</div>
-  <template v-for="stream in physicalStreams">
+  <template v-for="(stream, i) in physicalStreams">
     <div>Stream {{stream.name}} at <kbd>{{stream.tydiPath}}</kbd> that references <kbd>{{stream.dataPath}}</kbd></div>
+    <div>Data: <kbd>{{dataThings[i]}}</kbd></div>
     <div>
       <template v-for="item in stream.getItemsFlat()">
-        [<kbd>{{item.width}}</kbd> @ <kbd>{{item.dataPath}}</kbd>]
+        [<kbd>{{item.width}}</kbd> bits @ <kbd>{{item.dataPath}}</kbd>]
       </template>
     </div>
+    <hr class="my-3">
   </template>
-  <div>{{dataThings}}</div>
   <div>{{relativePaths}}</div>
 
 </template>
